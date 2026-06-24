81a67aff56748def40094e71f2e2816b28c5180d
Anis BEN JEMIA <anis.ben-jemia@etu.univ-lyon1.fr>
2026-06-24 16:36:37 +0200

    avancement actions

---
+++ actions/userActions.ts
@@ -0,0 +1,6 @@
+"use server";
+
+export default async function createUser(formData: FormData) {
+  console.log(formData);
+  console.log(123454345);
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,19 @@
+"use client";
 import { getMetadata } from "@/libs/metadata";
-
+import createUser from "@/actions/userActions";
+/*
 export async function generateStaticParams() {
   return getMetadata("contact");
 }
-
+*/
 export default function ContactPage() {
   return (
-    <div>ContactPage</div>
+    <div>
+      <div>ContactPage</div>
+      <form onSubmit={e => e.preventDefault() } action={createUser}>
+        <span className="text-xl">NOM</span>
+        <input name="name" className="bg-blue-300 w-[500px] h-[300px]" />
+      </form>
+    </div>
   );
 }
